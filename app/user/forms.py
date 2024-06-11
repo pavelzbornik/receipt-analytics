360d7a432fcaedd5ad3f7b04112be277137223e9
pavelzbornik <12965149+pavelzbornik@users.noreply.github.com>
2024-06-11 16:43:48 +0200

    pre-commit hooks

--- app/user/forms.py
+++ app/user/forms.py
@@ -47,6 +47,7 @@ class RegisterForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     """Edit profile form."""
+
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=3, max=25)]
     )
